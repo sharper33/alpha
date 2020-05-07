2b81d1bb075af00457e8a0c012d856e66539d187
sharper33 <60264039+sharper33@users.noreply.github.com>
2020-05-07 23:49:18 +0200

    Update windows_move.py

--- windows_move.py
+++ windows_move.py
@@ -2,7 +2,7 @@
 class WinMove:
 
     def __init__(self):
-        self.greet = "J. Krishnamurti is the shit"
+        self.greet = "J. Krishnamurti is the shit and very sexy"
 
     def greeting(self):
         return self.greet
